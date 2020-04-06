103830cf1c36585d14a6f1e454021b68e13a11c8
RiSecID <garudaindonesia710@gmail.com>
2020-04-06 23:09:58 +0700

    first commit

---
+++ userbot/modules/oi.py
@@ -0,0 +1,22 @@
+import asyncio
+import re
+import time
+from time import sleep
+from userbot import CMD_HELP, ZALG_LIST
+from userbot.events import register
+
+@register(outgoing=True, pattern='^.oi(?: |$)(.*)')
+async def typewriter(typew):
+	message = typew.pattern_match.group(1)
+	sleep(3)
+	await typew.edit("`Muka Gw Burik...`")
+	sleep(3)
+	await typew.edit("`Kayak Jemboot`")
+	sleep(1)
+	await typew.edit("`Muka Gw Burik Kayak Jemboot`")
+# Create by myself @localheart7
+
+
+
+
+
