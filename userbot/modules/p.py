7da2285d2d952534381714db696c39ba78045852
RiSecID <39178230+RiSecID@users.noreply.github.com>
2020-04-10 01:19:32 +0700

    Update p.py

--- userbot/modules/p.py
+++ userbot/modules/p.py
@@ -24,27 +24,11 @@ async def typewriter(typew):
 # Create by myself @RiSecID
 
 
-import asyncio
-import re
-import time
-from time import sleep
-from userbot import CMD_HELP, ZALG_LIST
-from userbot.events import register
-
-@register(outgoing=True, pattern='^P(?: |$)(.*)')
-async def typewriter(typew):
-	message = typew.pattern_match.group(1)
-	sleep(1)
-	await typew.edit("`Astaghfirulloh Ucapkan salam Dong...`")
-	sleep(1)
-	await typew.edit("`Assalamualaikum`")
-# Create by myself @RiSecID
-
 @register(outgoing=True, pattern='^kabeh(?: |$)(.*)')
 async def typewriter(typew):
 	message = typew.pattern_match.group(1)
 	sleep(1)
-	await typew.edit("@all"
+	await typew.edit("@all")
 # Create by myself @RiSecID
 
 
