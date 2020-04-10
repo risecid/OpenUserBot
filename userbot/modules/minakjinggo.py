9b7434b8b261361155080417e6abd3090d2f819d
RiSecID <39178230+RiSecID@users.noreply.github.com>
2020-04-10 07:37:14 +0700

    Update minakjinggo.py

--- userbot/modules/minakjinggo.py
+++ userbot/modules/minakjinggo.py
@@ -5,13 +5,7 @@ from time import sleep
 from userbot import CMD_HELP, ZALG_LIST
 from userbot.events import register
 
-@register(outgoing=True, pattern='^A(?: |$)(.*)')
-async def typewriter(typew):
-	message = typew.pattern_match.group(1)
-	await typew.edit("@all")
-# Create by myself @RiSecID
-
-@register(outgoing=True, pattern='^a(?: |$)(.*)')
+@register(outgoing=True, pattern='^.all(?: |$)(.*)')
 async def typewriter(typew):
 	message = typew.pattern_match.group(1)
 	await typew.edit("@all")
