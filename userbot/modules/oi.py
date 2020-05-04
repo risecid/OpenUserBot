7be963494310b0411a56eb4cb8fd29188ca67817
RiSecID <39178230+RiSecID@users.noreply.github.com>
2020-05-04 11:02:05 +0700

    Update oi.py

--- userbot/modules/oi.py
+++ userbot/modules/oi.py
@@ -16,7 +16,26 @@ async def typewriter(typew):
 	await typew.edit("`Muka Gw Gak Burik Kek Kalian`")
 # Create by myself @localheart
 
+@register(outgoing=True, pattern='^.io(?: |$)(.*)')
+async def typewriter(typew):
+	message = typew.pattern_match.group(1)
+	sleep(3)
+	await typew.edit("`Muka Gue Burik...`")
+	sleep(3)
+	await typew.edit("`Kayak Jemboot`")
+	sleep(1)
+	await typew.edit("`Muka Gw Burik Kek Jembott`")
+# Create by myself @localheart
 
-
+@register(outgoing=True, pattern='^.ll(?: |$)(.*)')
+async def typewriter(typew):
+	message = typew.pattern_match.group(1)
+	sleep(3)
+	await typew.edit("`Lucinta Luna Berbatang ...`")
+	sleep(3)
+	await typew.edit("`Tapi Gua sukaa`")
+	sleep(1)
+	await typew.edit("`I love memek Oplass`")
+# Create by myself @localheart
 
 
